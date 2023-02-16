c8e42dfcb36dc119f963a711eb200dd46428c60b
TejashreeKilledar <125530884+TejashreeKilledar@users.noreply.github.com>
2023-02-16 17:37:09 +0530

    Update LoginPageTestClass.java

--- FacebookAutomateTestApp/src/test/java/pkg1/LoginPageTestClass.java
+++ FacebookAutomateTestApp/src/test/java/pkg1/LoginPageTestClass.java
@@ -96,6 +96,8 @@ public void OpenBrowser(String browserName)
 	public void closeBrowser()
 	{
 		System.out.println("after method...");
+		System.out.println("after method...");
+		System.out.println("after method...");
 		driver.close();
 	}
 	/*@AfterClass
